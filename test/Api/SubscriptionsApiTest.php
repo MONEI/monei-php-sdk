51f71807317d18dc99847f18fa1065469301665f
Dmitriy <dn@monei.com>
2025-05-28 19:33:56 +0200

    chore: add empty code blocks to setup and teardown methods in SubscriptionsApiTest

--- test/Api/SubscriptionsApiTest.php
+++ test/Api/SubscriptionsApiTest.php
@@ -69,7 +69,9 @@ class SubscriptionsApiTest extends TestCase
     /**
      * Setup before running any test cases
      */
-    public static function setUpBeforeClass(): void {}
+    public static function setUpBeforeClass(): void
+    {
+    }
 
     /**
      * Setup before running each test case
@@ -109,7 +111,9 @@ class SubscriptionsApiTest extends TestCase
     /**
      * Clean up after running all test cases
      */
-    public static function tearDownAfterClass(): void {}
+    public static function tearDownAfterClass(): void
+    {
+    }
 
     /**
      * Test case for activate
