6ecef2c1be8b7e2656aeea2779c63df76251913e
Dmitriy <dn@monei.com>
2025-12-01 20:26:34 +0100

    chore: update OpenAPI document version to 1.8.0 and add new models for Apple Pay certificate management

--- test/Api/ApplePayCertificateApiTest.php
+++ test/Api/ApplePayCertificateApiTest.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * ApplePayCertificateApiTest
  * PHP version 8.1
@@ -42,7 +43,6 @@ use PHPUnit\Framework\TestCase;
  */
 class ApplePayCertificateApiTest extends TestCase
 {
-
     /**
      * Setup before running any test cases
      */
